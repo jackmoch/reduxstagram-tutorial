92fc6e2c7bba3626d645e20619dffa4e9ec903e1
Jack Mocherman <mochermanjack@gmail.com>
2016-10-08 15:28:23 -0500

    added photo grid and single photo components

---
+++ client/components/single.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react'
+import { Link } from 'react-router'
+
+const Single = React.createClass({
+	render() {
+		return (
+			<div className="single-photo">
+				Im the single grid
+			</div>	
+		)
+	}
+})
+
+export default Single
