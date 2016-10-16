35cf258494c6e4f34d598a4ae2301be4fe6b2520
Jack Mocherman <mochermanjack@gmail.com>
2016-10-15 20:36:00 -0500

    added Router to main js render

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -5,5 +5,20 @@ import { render } from 'react-dom'
 import css from './styles/style.styl'
 
 import Main from './components/main'
+import Single from './components/single'
+import PhotoGrid from './components/photogrid'
 
-render(<Main />, document.getElementById('root'))
+import { Router, Route, IndexRoute, browserHisory } from 'react-router'
+
+const router = (
+	<Router history={browserHisory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={PhotoGrid}></IndexRoute>
+			<Route path="/view/:postId" component={Single}></Route>
+		</Route>
+	</Router>
+)
+
+render(
+	router, 
+	document.getElementById('root'))
